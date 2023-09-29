c7b35ee7c8cf3b5015de23632de7eee0c90eb0e3
minimum-avail9 <144429813+minimum-avail9@users.noreply.github.com>
2023-09-29 11:28:16 +0300

    Завдання 2

--- Main.java
+++ Main.java
@@ -6,8 +6,15 @@ public class Main {
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
-        if (num1 > num2 && num1 > num3) System.out.println(num1);
-        if (num2 > num1 && num2 > num3) System.out.println(num2);
-        if (num3 > num2 && num3 > num1) System.out.println(num3);
+        int num4 = scanner.nextInt();
+        if (num1 < num2 && num1 < num3 && num1 < num4) System.out.println(num1);
+        if (num2 < num1 && num2 < num3 && num2 < num4) System.out.println(num2);
+        if (num3 < num2 && num3 < num1 && num3 < num4) System.out.println(num3);
+        if (num4 < num2 && num4 < num3 && num4 < num1) System.out.println(num4);
+
+
+
+
     }
 }
+
